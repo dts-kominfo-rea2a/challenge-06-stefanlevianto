53d3466adcce1f8e7f03ba1c86cd532a7e6c4030
stefanlevianto <stefanlevianto@gmail.com>
2022-06-08 18:03:53 +0700

    commit 1

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,34 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (callBack) => {
+  const word = [];
+  fs.readFile(file1, "utf8", (err, data1) => {
+    if (err) {
+      console.log("Terjadi Error pada file1");
+    } else {
+      let split = JSON.parse(data1).message.split(" ");
+      word.push(split[1]);
+      fs.readFile(file2, "utf8", (err, data2) => {
+        if (err) {
+          console.log("Terjadi Error pada file2");
+        } else {
+          let split = JSON.parse(data2)[0].message.split(" ");
+          word.push(split[1]);
+          fs.readFile(file3, "utf8", (err, data3) => {
+            if (err) {
+              console.log("terjadi Error pada file3");
+            } else {
+              let split = JSON.parse(data3)[0].data.message.split(" ");
+              word.push(split[1]);
+              callBack(err, word);
+            }
+          });
+        }
+      });
+    }
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
